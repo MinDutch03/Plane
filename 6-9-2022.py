59e2dbdbb2ba097cbae7d1be8e8db30f666a0ee1
Duc Nguyen <nguyenminhduc890@gmail.com>
2022-07-19 22:36:10 +0700

    Update 6-9-2022.py

--- 6-9-2022.py
+++ 6-9-2022.py
@@ -8,7 +8,7 @@ from sqlalchemy import except_, update
 
 AIO_FEED_ID = ""
 AIO_USERNAME = "namelessbtw"
-AIO_KEY = "aio_FZkg364PGZXNveNoT7PlKU9N4tsb"
+AIO_KEY = "aio_IqvG76uWuQBVVZupLeeKEvr2ZjM2"
 
 def  connected(client):
     print("Service connected")
